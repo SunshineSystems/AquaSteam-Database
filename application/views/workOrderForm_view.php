4bf2d081cc2927e2ed9a53c18f51fba682c71772
Joanna Koopmans <joanna.koopmans@hotmail.ca>
2013-03-12 17:36:13 -0600

    started formatting the layout

--- application/views/workOrderForm_view.php
+++ application/views/workOrderForm_view.php
@@ -15,11 +15,8 @@
 	    
    
 	    </head> 
+	    
 	    <body>
-	    	
-	    	
-	    	
-	<form class="form-horizontal">
 
     		<div class="control-group">
     			<label class="control-label" for="workOrderID">Work Order ID</label>
@@ -93,117 +90,101 @@
     					<input type="text" id="custCPhone">
     				</div>
     		</div>
-    		
-    	
-    
-    <div class="controls">
-    <label class="checkbox inline">
-    <input type="checkbox"> Cash
-    </label>
-    <label class="checkbox inline">
-    <input type="checkbox"> Cheque
-    </label>
-    <label class="checkbox inline">
-    <input type="checkbox"> Credit Card
-    </label>
-    <label class="checkbox inline">
-    <input type="checkbox"> Charge
-    </label>
-    <label class="checkbox inline">
-    <input type="checkbox"> Other
-    <input type="text" id="payOther">
-    </label>
-    </div>
-    
-    
-    <div class="controls">
-    <label class="checkbox inline">
-    <input type="checkbox"> RX
-    </label>
-    <label class="checkbox inline">
-    <input type="checkbox"> Fan
-    </label>
-    <label class="checkbox inline">
-    <input type="checkbox"> Rake
-    </label>
-    <label class="checkbox inline">
-    <input type="checkbox"> Pad
-    </label>
-    <label class="checkbox inline">
-    <input type="checkbox"> Encapsulate
-    </label>
-    
-    <label class="checkbox inline">
-    <input type="checkbox"> Info Form
-    </label>
-    
-    </div>
-    
+
 
 		<div class="control-group">
-    			<label class="control-label" for="workOrderGift">Gift</label>
-    				<div class="controls">
-    					<input type="text" id="workOrderGift">
-    				</div> 
-    	</div>   		
-    		
-	
-	
-	
-	
-	<div class="control-group">
-    	<label class="control-label" for="workOrderSqFeet">Sq Feet</label>
-   			<div class="controls">
- 				<input type="text" id="workOderSqFeet">
-  			</div>
- 	</div>
+    		<label class="control-label" for="workOrderGift">Gift</label>
+    			<div class="controls">
+    				<input type="text" id="workOrderGift">
+    			</div> 
+    	</div> 
+    	 	
+    <div class="control-group">
+		<label class="control-label" for="workOrderDate">Date </label>
+			<input type="text" id="datepicker" placeholder="mm/dd/yyyy"/>
+	</div>  		
 
  	<div class="control-group">
     	<label class="control-label" for="workOrderDiscount">Discount</label>
    			<div class="controls">
  				<input type="text" id="workOderDiscount">
-  			
-<select class="input-small">
-		<option>%</option>
-		<option>$</option>
-	</select>
+  				<select class="input-small">
+					<option>%</option>
+					<option>$</option>
+				</select>
+   			</div>
+    </div>
+	
+	<div class="checkbox">
+	<div class="controls">
+    	<label>
+    		<input type="checkbox"> Cash
+    	</label>
+    	<label>
+    		<input type="checkbox"> Cheque
+    	</label>
+    	<label>
+    		<input type="checkbox"> Credit Card
+    	</label>
+    	<label>
+    		<input type="checkbox"> Charge
+    	</label>
+    	<label>
+    		<input type="checkbox"> Other
+    		<input type="text" id="payOther">
+    	</label>
     </div>
     </div>
- 	
-    		
-<div class="control-group">
-	<label class="control-label" for="workOrderDate">Date </label>
-	<input type="text" id="datepicker" placeholder="mm/dd/yyyy"/>
+    
+    <div class="checkbox"gn="right">
+    <div class="controls">
+    	<label>
+    		<input type="checkbox"> RX
+   		 </label>
+    	<label>
+    		<input type="checkbox"> Fan
+    	</label>
+    	<label>
+    		<input type="checkbox"> Rake
+    	</label>
+    	<label>
+    		<input type="checkbox"> Pad
+    	</label>
+    	<label>
+    		<input type="checkbox"> Encapsulate
+    	</label>
+    	<label>
+    		<input type="checkbox"> Info Form
+    	</label>
+    </div>
 	</div>
 	
-	
 	<div id="tabs">
-<ul>
-<li><a href="#carpetTab">Carpet</a></li>
-<li><a href="#upholsteryTab">Upholstery</a></li>
-<li><a href="#stainGuardTab">Stain Guard</a></li>
-<li><a href="#spotsTab">Spots</a></li>
-<li><a href="#travelTab">Travel</a></li>
-<li><a href="#notesTab">Notes</a></li>
-
-</ul>
-<div id="carpetTab">
- <table border="1">
-        <thead>
-            <tr>
-            	<th>Identifier *not sure</th>
-                <th>Description</th>
-                <th>Color/Type</th>                     
-                <th>Length</th>
-                <th>Width</th>
-                <th>Sq Feet</th>
-                <th>Quantity</th>
-                <th>Unit Price</th>
-                <th>Extended Price</th>
-                <th>Total Price</th>
-            </tr>
-        </thead>
-        <tbody>
+		<ul>
+			<li><a href="#carpetTab">Carpet</a></li>
+			<li><a href="#upholsteryTab">Upholstery</a></li>
+			<li><a href="#stainGuardTab">Stain Guard</a></li>
+			<li><a href="#spotsTab">Spots</a></li>
+			<li><a href="#travelTab">Travel</a></li>
+			<li><a href="#notesTab">Notes</a></li>
+		</ul>
+		<div id="carpetTab">
+			<table border="1">
+        		<thead>
+            		<tr>
+            			<th>Identifier *not sure</th>
+                		<th>Description</th>
+                		<th>Color/Type</th>                     
+                		<th>Length</th>
+                		<th>Width</th>
+                		<th>Sq Feet</th>
+                		<th>Quantity</th>
+                		<th>Unit Price</th>
+                		<th>Extended Price</th>
+                		<th>Total Price</th>
+            		</tr>
+        		</thead>
+        	<tbody>
             <tr>         
                 <!-- The "identifier" class makes it so we have an id
                     to pass to our ajax script so we know what to change -->       
@@ -219,32 +200,37 @@
                 <td>Total Price not editable</td>
                 
             </tr>                   
-        </tbody>
-    </table>
-</div>
-<div id="upholsteryTab">
-<p>Morbi tincidunt, dui sit amet facilisis feugiat, odio metus gravida ante, ut pharetra massa metus id nunc. Duis scelerisque molestie turpis. Sed fringilla, massa eget luctus malesuada, metus eros molestie lectus, ut tempus eros massa ut dolor. Aenean aliquet fringilla sem. Suspendisse sed ligula in ligula suscipit aliquam. Praesent in eros vestibulum mi adipiscing adipiscing. Morbi facilisis. Curabitur ornare consequat nunc. Aenean vel metus. Ut posuere viverra nulla. Aliquam erat volutpat. Pellentesque convallis. Maecenas feugiat, tellus pellentesque pretium posuere, felis lorem euismod felis, eu ornare leo nisi vel felis. Mauris consectetur tortor et purus.</p>
-</div>
-<div id="stainGuardTab">
-<p>Mauris eleifend est et turpis. Duis id erat. Suspendisse potenti. Aliquam vulputate, pede vel vehicula accumsan, mi neque rutrum erat, eu congue orci lorem eget lorem. Vestibulum non ante. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Fusce sodales. Quisque eu urna vel enim commodo pellentesque. Praesent eu risus hendrerit ligula tempus pretium. Curabitur lorem enim, pretium nec, feugiat nec, luctus a, lacus.</p>
-<p>Duis cursus. Maecenas ligula eros, blandit nec, pharetra at, semper at, magna. Nullam ac lacus. Nulla facilisi. Praesent viverra justo vitae neque. Praesent blandit adipiscing velit. Suspendisse potenti. Donec mattis, pede vel pharetra blandit, magna ligula faucibus eros, id euismod lacus dolor eget odio. Nam scelerisque. Donec non libero sed nulla mattis commodo. Ut sagittis. Donec nisi lectus, feugiat porttitor, tempor ac, tempor vitae, pede. Aenean vehicula velit eu tellus interdum rutrum. Maecenas commodo. Pellentesque nec elit. Fusce in lacus. Vivamus a libero vitae lectus hendrerit hendrerit.</p>
-</div>
-<div id="spotsTab">
-<p>Mauris eleifend est et turpis. Duis id erat. Suspendisse potenti. Aliquam vulputate, pede vel vehicula accumsan, mi neque rutrum erat, eu congue orci lorem eget lorem. Vestibulum non ante. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Fusce sodales. Quisque eu urna vel enim commodo pellentesque. Praesent eu risus hendrerit ligula tempus pretium. Curabitur lorem enim, pretium nec, feugiat nec, luctus a, lacus.</p>
-<p>Duis cursus. Maecenas ligula eros, blandit nec, pharetra at, semper at, magna. Nullam ac lacus. Nulla facilisi. Praesent viverra justo vitae neque. Praesent blandit adipiscing velit. Suspendisse potenti. Donec mattis, pede vel pharetra blandit, magna ligula faucibus eros, id euismod lacus dolor eget odio. Nam scelerisque. Donec non libero sed nulla mattis commodo. Ut sagittis. Donec nisi lectus, feugiat porttitor, tempor ac, tempor vitae, pede. Aenean vehicula velit eu tellus interdum rutrum. Maecenas commodo. Pellentesque nec elit. Fusce in lacus. Vivamus a libero vitae lectus hendrerit hendrerit.</p>
-</div>
-<div id="travelTab">
-<p>Mauris eleifend est et turpis. Duis id erat. Suspendisse potenti. Aliquam vulputate, pede vel vehicula accumsan, mi neque rutrum erat, eu congue orci lorem eget lorem. Vestibulum non ante. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Fusce sodales. Quisque eu urna vel enim commodo pellentesque. Praesent eu risus hendrerit ligula tempus pretium. Curabitur lorem enim, pretium nec, feugiat nec, luctus a, lacus.</p>
-<p>Duis cursus. Maecenas ligula eros, blandit nec, pharetra at, semper at, magna. Nullam ac lacus. Nulla facilisi. Praesent viverra justo vitae neque. Praesent blandit adipiscing velit. Suspendisse potenti. Donec mattis, pede vel pharetra blandit, magna ligula faucibus eros, id euismod lacus dolor eget odio. Nam scelerisque. Donec non libero sed nulla mattis commodo. Ut sagittis. Donec nisi lectus, feugiat porttitor, tempor ac, tempor vitae, pede. Aenean vehicula velit eu tellus interdum rutrum. Maecenas commodo. Pellentesque nec elit. Fusce in lacus. Vivamus a libero vitae lectus hendrerit hendrerit.</p>
-</div>
-<div id="notesTab">
-<p>Mauris eleifend est et turpis. Duis id erat. Suspendisse potenti. Aliquam vulputate, pede vel vehicula accumsan, mi neque rutrum erat, eu congue orci lorem eget lorem. Vestibulum non ante. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Fusce sodales. Quisque eu urna vel enim commodo pellentesque. Praesent eu risus hendrerit ligula tempus pretium. Curabitur lorem enim, pretium nec, feugiat nec, luctus a, lacus.</p>
-<p>Duis cursus. Maecenas ligula eros, blandit nec, pharetra at, semper at, magna. Nullam ac lacus. Nulla facilisi. Praesent viverra justo vitae neque. Praesent blandit adipiscing velit. Suspendisse potenti. Donec mattis, pede vel pharetra blandit, magna ligula faucibus eros, id euismod lacus dolor eget odio. Nam scelerisque. Donec non libero sed nulla mattis commodo. Ut sagittis. Donec nisi lectus, feugiat porttitor, tempor ac, tempor vitae, pede. Aenean vehicula velit eu tellus interdum rutrum. Maecenas commodo. Pellentesque nec elit. Fusce in lacus. Vivamus a libero vitae lectus hendrerit hendrerit.</p>
-</div>
+        	</tbody>
+    		</table>
+	</div>
+	
+	<div id="upholsteryTab">
+		<p>Morbi tincidunt, dui sit amet facilisis feugiat, odio metus gravida ante, ut pharetra massa metus id nunc. Duis scelerisque molestie turpis. Sed fringilla, massa eget luctus malesuada, metus eros molestie lectus, ut tempus eros massa ut dolor. Aenean aliquet fringilla sem. Suspendisse sed ligula in ligula suscipit aliquam. Praesent in eros vestibulum mi adipiscing adipiscing. Morbi facilisis. Curabitur ornare consequat nunc. Aenean vel metus. Ut posuere viverra nulla. Aliquam erat volutpat. Pellentesque convallis. Maecenas feugiat, tellus pellentesque pretium posuere, felis lorem euismod felis, eu ornare leo nisi vel felis. Mauris consectetur tortor et purus.</p>
+	</div>
+	
+	<div id="stainGuardTab">
+		<p>Mauris eleifend est et turpis. Duis id erat. Suspendisse potenti. Aliquam vulputate, pede vel vehicula accumsan, mi neque rutrum erat, eu congue orci lorem eget lorem. Vestibulum non ante. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Fusce sodales. Quisque eu urna vel enim commodo pellentesque. Praesent eu risus hendrerit ligula tempus pretium. Curabitur lorem enim, pretium nec, feugiat nec, luctus a, lacus.</p>
+		<p>Duis cursus. Maecenas ligula eros, blandit nec, pharetra at, semper at, magna. Nullam ac lacus. Nulla facilisi. Praesent viverra justo vitae neque. Praesent blandit adipiscing velit. Suspendisse potenti. Donec mattis, pede vel pharetra blandit, magna ligula faucibus eros, id euismod lacus dolor eget odio. Nam scelerisque. Donec non libero sed nulla mattis commodo. Ut sagittis. Donec nisi lectus, feugiat porttitor, tempor ac, tempor vitae, pede. Aenean vehicula velit eu tellus interdum rutrum. Maecenas commodo. Pellentesque nec elit. Fusce in lacus. Vivamus a libero vitae lectus hendrerit hendrerit.</p>
+	</div>
+
+	<div id="spotsTab">
+		<p>Mauris eleifend est et turpis. Duis id erat. Suspendisse potenti. Aliquam vulputate, pede vel vehicula accumsan, mi neque rutrum erat, eu congue orci lorem eget lorem. Vestibulum non ante. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Fusce sodales. Quisque eu urna vel enim commodo pellentesque. Praesent eu risus hendrerit ligula tempus pretium. Curabitur lorem enim, pretium nec, feugiat nec, luctus a, lacus.</p>
+		<p>Duis cursus. Maecenas ligula eros, blandit nec, pharetra at, semper at, magna. Nullam ac lacus. Nulla facilisi. Praesent viverra justo vitae neque. Praesent blandit adipiscing velit. Suspendisse potenti. Donec mattis, pede vel pharetra blandit, magna ligula faucibus eros, id euismod lacus dolor eget odio. Nam scelerisque. Donec non libero sed nulla mattis commodo. Ut sagittis. Donec nisi lectus, feugiat porttitor, tempor ac, tempor vitae, pede. Aenean vehicula velit eu tellus interdum rutrum. Maecenas commodo. Pellentesque nec elit. Fusce in lacus. Vivamus a libero vitae lectus hendrerit hendrerit.</p>
+	</div>
+
+	<div id="travelTab">
+		<p>Mauris eleifend est et turpis. Duis id erat. Suspendisse potenti. Aliquam vulputate, pede vel vehicula accumsan, mi neque rutrum erat, eu congue orci lorem eget lorem. Vestibulum non ante. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Fusce sodales. Quisque eu urna vel enim commodo pellentesque. Praesent eu risus hendrerit ligula tempus pretium. Curabitur lorem enim, pretium nec, feugiat nec, luctus a, lacus.</p>
+		<p>Duis cursus. Maecenas ligula eros, blandit nec, pharetra at, semper at, magna. Nullam ac lacus. Nulla facilisi. Praesent viverra justo vitae neque. Praesent blandit adipiscing velit. Suspendisse potenti. Donec mattis, pede vel pharetra blandit, magna ligula faucibus eros, id euismod lacus dolor eget odio. Nam scelerisque. Donec non libero sed nulla mattis commodo. Ut sagittis. Donec nisi lectus, feugiat porttitor, tempor ac, tempor vitae, pede. Aenean vehicula velit eu tellus interdum rutrum. Maecenas commodo. Pellentesque nec elit. Fusce in lacus. Vivamus a libero vitae lectus hendrerit hendrerit.</p>
+	</div>
+
+	<div id="notesTab">
+		<p>Mauris eleifend est et turpis. Duis id erat. Suspendisse potenti. Aliquam vulputate, pede vel vehicula accumsan, mi neque rutrum erat, eu congue orci lorem eget lorem. Vestibulum non ante. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Fusce sodales. Quisque eu urna vel enim commodo pellentesque. Praesent eu risus hendrerit ligula tempus pretium. Curabitur lorem enim, pretium nec, feugiat nec, luctus a, lacus.</p>
+		<p>Duis cursus. Maecenas ligula eros, blandit nec, pharetra at, semper at, magna. Nullam ac lacus. Nulla facilisi. Praesent viverra justo vitae neque. Praesent blandit adipiscing velit. Suspendisse potenti. Donec mattis, pede vel pharetra blandit, magna ligula faucibus eros, id euismod lacus dolor eget odio. Nam scelerisque. Donec non libero sed nulla mattis commodo. Ut sagittis. Donec nisi lectus, feugiat porttitor, tempor ac, tempor vitae, pede. Aenean vehicula velit eu tellus interdum rutrum. Maecenas commodo. Pellentesque nec elit. Fusce in lacus. Vivamus a libero vitae lectus hendrerit hendrerit.</p>
+	</div>
 </div>
 
 
-    </form>
+    
 
 <script src="<?php echo $home?>js/workorderform.js"></script>
 
