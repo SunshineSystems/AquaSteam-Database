cd0c7e5a05a0975c5b8a490028286b5dec29e79d
Joanna Koopmans <joanna.koopmans@hotmail.ca>
2013-03-25 14:13:17 -0600

    Fixed the work order links

--- application/views/empMainMenu_view.php
+++ application/views/empMainMenu_view.php
@@ -7,7 +7,7 @@
 		     	<h2>Main Menu</h2>
 		    
 		     	<a class="btn" href="<?php echo $home?>index.php/customer">Customers</a><br>
-		     	<a class="btn" href="<?php echo $home?>index.php/workOrder">Work Orders</a><br>
+		     	<a class="btn" href="<?php echo $home?>index.php/workOrderSearch">Work Orders</a><br>
 		     	<a class="btn" href="<?php echo $home?>documents/Work Order.pdf">Print Blank Work Order</a><br>
 		     	<a class="btn" href="<?php echo $home?>index.php/changePassword">Change Password</a><br>
 		     	<a class="btn" href="https://www.google.com/calendar" target="_blank">Google Calendar</a><br>
